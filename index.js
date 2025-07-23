e4940a215c0d67f416b41dcc0cb1b0cea36e8967
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-07-23 09:59:26 +0600

    added api for check the user is admin or not

--- index.js
+++ index.js
@@ -147,6 +147,19 @@ app.patch('/users/:id', async(req, res)=>{
 
 })
 
+app.get('/users/admin/:email', verifyToken, async(req, res)=>{
+ const email = req.params.email;
+ if(email !== req.decoded.email){
+  return res.status(403).send({message: "Forbidden access"})
+ }
+ const query = {email: email};
+ const user = await userCollection.findOne(query);
+ let admin = false;
+ if(user){
+  admin = user?.role === 'admin'
+ }
+ res.send({admin})
+})
 
 
 
